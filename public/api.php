050bfb930e5453fd2a2dd2b1e14a7ac2fc8b9f13
VinetaVevere <vineta.vevere@zzdats.lv>
2022-05-19 21:41:36 +0300

    Refactored api.php- less rows

--- public/api.php
+++ public/api.php
@@ -9,20 +9,11 @@ header('Content-Type: application/json');
 $output = ['status' => false];
 if (isset($_GET['name']) && is_string($_GET['name'])) {
     $comment_helper = new Comments();
-    if ($_GET['name'] === 'add-comment') {
-        $output = $comment_helper->add();
-    }
-    elseif ($_GET['name'] === 'update-comment') {
-        $output = $comment_helper->update();
-    }
-    elseif ($_GET['name'] === 'get-comments') {
-        $output = $comment_helper->getAll();
-    }
-    elseif ($_GET['name'] === 'delete-comment') {
-        $output = $comment_helper->delete();
-    }
-    elseif ($_GET['name'] === 'get-comment') {
-        $output = $comment_helper->get();
+    $api_name = ['add-comment', 'update-comment', 'getAll-comment', 'delete-comment', 'get-comment'];
+
+    if (array_key_exists($_GET['name'], array_flip($api_name))) {
+        $arr = explode('-', $_GET['name']);
+        $output = $comment_helper->{$arr[0]}();
     }
 }
 
