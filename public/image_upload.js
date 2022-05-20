342ce99a0e95753e641561ef4b62362c2a04f28a
VinetaVevere <vineta.vevere@zzdats.lv>
2022-05-20 21:21:07 +0300

    images display

---
+++ public/image_upload.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('comments_form');
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+    xhttp.postForm(this, function (response) {
+        console.log(response);
+    });
+
+}
